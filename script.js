c5132df600c5136dd4809381053dd35931c42fec
danierokon <danierokon@gmail.com>
2022-10-16 19:49:04 +0800

    hover effect done

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const container = document.querySelector('#container');
+const grids = [];
 
 function createGrid(input){    
     for(i=0; i<(input*input); i++){
         const grid = document.createElement('div');
         grid.classList.add('grid');
-        container.appendChild(grid);        
+        container.appendChild(grid);
+        grids.push(grid);
         //console.log(i);
     }
     container.style.cssText = 
     `grid-template-columns: repeat(${input},1fr);`    
 }
-
-createGrid(16);
+createGrid(16); //initialize
+for (grid of grids){
+    grid.addEventListener('mouseover',hoverEffect);
+}
+function hoverEffect(e){
+    this.classList.add('hover')    
+}
