c5dc639d1f1449c95e9c7ed4542cfd1f1b732cee
danierokon <danierokon@gmail.com>
2022-10-16 21:42:15 +0800

    added button to receive input, function to remove old grids

--- script.js
+++ script.js
@@ -10,12 +10,38 @@ function createGrid(input){
         //console.log(i);
     }
     container.style.cssText = 
-    `grid-template-columns: repeat(${input},1fr);`    
+    `grid-template-columns: repeat(${input},1fr);`   
+    for (grid of grids){
+        grid.addEventListener('mouseover',hoverEffect);
+    } 
 }
 createGrid(16); //initialize
-for (grid of grids){
-    grid.addEventListener('mouseover',hoverEffect);
-}
+
 function hoverEffect(e){
     this.classList.add('hover')    
+}
+
+const inputButton = document.querySelector('#input');
+    inputButton.addEventListener('click',getInput);    
+function getInput(){    
+     let gridInput = prompt('Please enter an integer from 1-100')
+     let status = false;
+    while (status === false ){
+        if (gridInput > 0 &&
+            gridInput < 101){
+            break;
+            }
+        else{gridInput = prompt('Please enter an integer from 1-100!')}                      
+    }
+    //console.log(gridInput);
+    //remove old grids
+    removeOldGrids();
+    createGrid(gridInput);
+};
+
+function removeOldGrids(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    grids.splice(0,grids.length);   
 }
